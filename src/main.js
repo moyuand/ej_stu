bdbb3818d3711a2a36864ff839f8af892c7904f8
moyuand <sxaufujie@163.com>
2020-01-04 08:42:36 +0800

    first

--- src/main.js
+++ src/main.js
@@ -24,16 +24,16 @@ import '@/permission' // permission control
  * please remove it before going online! ! !
  */
 import { mockXHR } from '../mock'
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   mockXHR()
-}
+// }
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false 
 
 new Vue({
   el: '#app',
