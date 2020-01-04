b5efb603f7e9a4d4629ff4f4551027551a2c6e52
rentonghui1999 <480954081@qq.com>
2020-01-04 16:49:45 +0800

    comment，address,gai

--- src/pages/comment/List.vue
+++ src/pages/comment/List.vue
@@ -10,7 +10,7 @@
       <el-table-column prop="id" label="编号"></el-table-column>
       <el-table-column prop="content" label="内容"></el-table-column>
       <el-table-column prop="commentTime" label="评论时间"></el-table-column>
-       <el-table-column prop="orderId" label="订单ID"></el-table-column>
+       <el-table-column prop="orderId" label="订单"></el-table-column>
       <el-table-column label="操作">
         <template v-slot="slot">
           <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
@@ -24,24 +24,18 @@
     <!-- /分页结束 -->
     <!-- 模态框 -->
     <el-dialog
-      title="录入评论信息"
+      title="录入订单信息"
       :visible.sync="visible"
       width="60%">
         ---{{form}}
       <el-form :model="form" label-width="80px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input type="password" v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item label="内容">
-          <el-input v-model="form.content"></el-input>
-        </el-form-item>
-        <el-form-item label="评论时间">
+     <el-form-item label="评论内容">
+                    <el-input v-model="form.content" type="textarea" :rows="2" placeholder="请输入内容"></el-input>
+                </el-form-item>
+                <el-form-item label="评论时间">
           <el-input v-model="form.commentTime"></el-input>
         </el-form-item>
-        <el-form-item label="订单ID">
+        <el-form-item label="订单">
           <el-input v-model="form.orderId"></el-input>
         </el-form-item>
       </el-form>
@@ -75,7 +69,7 @@ export default {
       // request.post(url,this.form)
       // 查询字符串 type=comment&age=12
       // 通过request与后台进行交互，并且要携带参数
-      let url = "http://localhost:6677/comment/findAll";
+      let url = "http://localhost:6677/comment/saveOrUpdate";
       request({
         url,
         method:"POST",
